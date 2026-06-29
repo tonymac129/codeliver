fc19200e1e38c39a90e828ead977013aa8690c46
tonymac129 <tonyhsu129@gmail.com>
2026-06-28 22:48:04 -0400

    Resturcture

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-black font-inter">
-      <h1 className="text-5xl text-white">Codeliver</h1>{" "}
-    </div>
-  );
-}
