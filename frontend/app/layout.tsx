1eeefcdfc459f138b48b3140e3519e3bfac7f679
tonymac129 <tonyhsu129@gmail.com>
2026-06-28 22:47:09 -0400

    delete

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Welcome to Codeliver!",
+  description:
+    "Codeliver: sharing code, chatting, and collaborating made simple.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black font-inter">
+      <h1 className="text-5xl text-white">Codeliver</h1>{" "}
+    </div>
+  );
+}
